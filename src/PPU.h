6cabaafad0b7eae5ed52e616f7506167f75e6732
danielelbaz7 <175679070+danielelbaz7@users.noreply.github.com>
2026-01-06 04:05:48 -0500

    created drawsprites function, created spritesenabled lambda, need to implement the former

--- src/PPU.h
+++ src/PPU.h
@@ -29,13 +29,14 @@ private:
 
     void DrawBackground(uint32_t *scanline);
     void DrawWindow(uint32_t *scanline);
+    void DrawSprites(uint32_t *scanline);
 
     //lambda function that returns the current sprite height at any point
     [[nodiscard]] uint8_t spriteHeight() const { return ((Read(0xFF40) & 0x04) >> 2u == 1) ? 16 : 8; }
     //checks bit 3, to tell bg which address to use
     [[nodiscard]] bool LCDCBit3Tilemaps() const { return (((Read(0xFF40) & 0x08) >> 3u) == 1) ? true : false; }
-    [[nodiscard]] bool windowEnabled() const { return ((((Read(0xFF40) & 0x10) >> 4u) == 1) ? true : false)
-        && (((Read(0xFF40) & 0x01) == 1) ? true : false); }
+    [[nodiscard]] bool windowEnabled() const { return ((((Read(0xFF40) & 0x10) >> 4u) == 1) ? true : false); }
+    [[nodiscard]] bool spritesEnabled() const { return ((((Read(0xFF40) & 0x01)) == 1) ? true : false); }
     [[nodiscard]] uint8_t windowStartY() const { return Read(0xFF4A); }
     [[nodiscard]] uint8_t windowStartX() const { return Read(0xFF4B); }
     //same as lcdcbit3 tilemaps, just for window
